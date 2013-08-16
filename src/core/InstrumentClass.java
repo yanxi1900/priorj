16d91611ff6ccd2f7444e2ad658c85844180b1ea
Samuel Santos <thiagsamuel@gmail.com>
2013-08-16 03:12:12 -0300

    Update InstrumentClass.java
    conflict merge resolved!!!

--- src/core/InstrumentClass.java
+++ src/core/InstrumentClass.java
@@ -235,12 +235,9 @@ public class InstrumentClass {
         if (stmts != null) {
             for (int i = 0; i < stmts.size(); i++) {
                 Statement stm = stmts.get(i);
-<<<<<<< HEAD
-              
-                if(i != 0 || !(stmts.get(i).toString().contains("super") || stmts.get(i).toString().contains("this")) )
-=======
+
                 if(i != 0 || (stmts.get(i).toString().contains("super") || stmts.get(i).toString().contains("this")))
->>>>>>> 7792b8211d9726ecfbb151ef880ae710d292880b
+
                 	novaLista.add(watchStatement);
                 stm = instrumentarBody(stm, watchStatement);
                 novaLista.add(stm);
