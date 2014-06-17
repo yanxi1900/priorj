46a3c074b3b21f62ba2a8750492102cc1a6139e7
samueltcsantos <thiagsamuel@gmail.com>
2014-06-16 23:43:47 -0300

    adding new execution order report.

--- src/report/GenerateCoverageReport.java
+++ src/report/GenerateCoverageReport.java
@@ -3,7 +3,7 @@ package report;
 /*
 * PriorJ: JUnit Test Case Prioritization.
 * 
-* Copyright (C) 2012-2013  Samuel T. C. Santos
+* Copyright (C) 2012-2014  SPLab
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
